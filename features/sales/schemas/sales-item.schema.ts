e9fe3eb8b87d8611aa859dd9ef10f2159f86c621
Kingshuk3006 <kingsarkar3006@gmail.com>
2026-07-13 01:05:09 +0530

    build error fixed

--- features/sales/schemas/sales-item.schema.ts
+++ features/sales/schemas/sales-item.schema.ts
@@ -21,7 +21,6 @@ export const saleItems = pgTable(
     sellingPrice: numeric("selling_price", {
       precision: 12,
       scale: 2,
-      mode: "number",
     }).notNull(),
 
     lineTotal: numeric("line_total", {
